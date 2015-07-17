8e4e1deaa8d43cdc0b52c416d15d33c75a7b912d
Kevin Xu <kevin.xu@asb.co.nz>
2015-07-17 15:16:06 +1200

    fix indentation

--- app/app.js
+++ app/app.js
@@ -1,24 +1,24 @@
 (function () {
-  'use strict';
+    'use strict';
 
-  angular.module('app', [])
-    .constant('config', {
-      api: {
-        endPoint: 'https://api.github.com/',
-        usersUrl: 'users/',
-        reposUrl: 'repos/',
-        reposParam: '/repos',
-        langsParam: '/languages'
-      }
-    });
+    angular.module('app', [])
+        .constant('config', {
+            api: {
+                endPoint: 'https://api.github.com/',
+                usersUrl: 'users/',
+                reposUrl: 'repos/',
+                reposParam: '/repos',
+                langsParam: '/languages'
+            }
+        });
 
-  // User details
-  require('./scripts/userDetails/user.details.directive.js');
-  // User panel
-  require('./scripts/userPanel/user.panel.controller.js');
-  require('./scripts/userPanel/user.panel.directive.js');
-  // User repository
-  require('./scripts/userRepository/user.repository.controller.js');
-  require('./scripts/userRepository/user.repository.directive.js');
+    // User details
+    require('./scripts/userDetails/user.details.directive.js');
+    // User panel
+    require('./scripts/userPanel/user.panel.controller.js');
+    require('./scripts/userPanel/user.panel.directive.js');
+    // User repository
+    require('./scripts/userRepository/user.repository.controller.js');
+    require('./scripts/userRepository/user.repository.directive.js');
   
 })();
